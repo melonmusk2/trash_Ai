3b15fa6e7520ae43aab12a91be59483d94ed6b5a
Jeremiah Hetzel <117078336+melonmusk2@users.noreply.github.com>
2025-07-04 16:48:42 +0200

    Delete response.py

--- response.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-files = {'file': open('example.jpg', 'rb')}
-response = requests.post("https://YOUR_USERNAME-your-space-name.hf.space/run/predict", files=files)
-
-print(response.json())
